357b97fdf4d8309f5f39b0a9d10d2b39133bebfa
nicolegros <nicolaslegros98@gmail.com>
2024-04-10 09:25:09 -0400

    :recycle: Changed naming of generated `.csv` files

--- setup.py
+++ setup.py
@@ -2,15 +2,27 @@ from src.fileloader import processfiles
 
 
 def extractdata():
-    processfiles('./data/raw/mongo/mongo_3_10_90', './data/clean/mongo/3_10_90_load.csv', 'load')
-    processfiles('./data/raw/mongo/mongo_3_50_50', './data/clean/mongo/3_50_50_load.csv', 'load')
-    processfiles('./data/raw/mongo/mongo_5_10_90', './data/clean/mongo/5_10_90_load.csv', 'load')
-    processfiles('./data/raw/mongo/mongo_5_50_50', './data/clean/mongo/5_50_50_load.csv', 'load')
+    # Mongo
+    processfiles('./data/raw/mongo/mongo_3_10_90', './data/clean/mongo/load_3_10_90.csv', 'load')
+    processfiles('./data/raw/mongo/mongo_3_50_50', './data/clean/mongo/load_3_50_50.csv', 'load')
+    processfiles('./data/raw/mongo/mongo_5_10_90', './data/clean/mongo/load_5_10_90.csv', 'load')
+    processfiles('./data/raw/mongo/mongo_5_50_50', './data/clean/mongo/load_5_50_50.csv', 'load')
 
-    processfiles('./data/raw/redis/redis_3_10_90', './data/clean/redis/3_10_90_load.csv', 'load')
-    processfiles('./data/raw/redis/redis_3_50_50', './data/clean/redis/3_50_50_load.csv', 'load')
-    processfiles('./data/raw/redis/redis_5_10_90', './data/clean/redis/5_10_90_load.csv', 'load')
-    processfiles('./data/raw/redis/redis_5_50_50', './data/clean/redis/5_50_50_load.csv', 'load')
+    processfiles('./data/raw/mongo/mongo_3_10_90', './data/clean/mongo/run_3_10_90.csv', 'run')
+    processfiles('./data/raw/mongo/mongo_3_50_50', './data/clean/mongo/run_3_50_50.csv', 'run')
+    processfiles('./data/raw/mongo/mongo_5_10_90', './data/clean/mongo/run_5_10_90.csv', 'run')
+    processfiles('./data/raw/mongo/mongo_5_50_50', './data/clean/mongo/run_5_50_50.csv', 'run')
+
+    # Redis
+    processfiles('./data/raw/redis/redis_3_10_90', './data/clean/redis/load_3_10_90.csv', 'load')
+    processfiles('./data/raw/redis/redis_3_50_50', './data/clean/redis/load_3_50_50.csv', 'load')
+    processfiles('./data/raw/redis/redis_5_10_90', './data/clean/redis/load_5_10_90.csv', 'load')
+    processfiles('./data/raw/redis/redis_5_50_50', './data/clean/redis/load_5_50_50.csv', 'load')
+
+    processfiles('./data/raw/redis/redis_3_10_90', './data/clean/redis/run_3_10_90.csv', 'run')
+    processfiles('./data/raw/redis/redis_3_50_50', './data/clean/redis/run_3_50_50.csv', 'run')
+    processfiles('./data/raw/redis/redis_5_10_90', './data/clean/redis/run_5_10_90.csv', 'run')
+    processfiles('./data/raw/redis/redis_5_50_50', './data/clean/redis/run_5_50_50.csv', 'run')
 
 
 if __name__ == '__main__':
